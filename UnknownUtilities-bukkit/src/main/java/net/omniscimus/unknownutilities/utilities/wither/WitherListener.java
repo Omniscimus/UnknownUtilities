9fde74c4a765ca7e1ba5743295a5d864e3e39be1
Omniscimus <omniscimus@icloud.com>
2016-04-03 11:49:57 +0200

    Keep track of who spawned the wither

--- UnknownUtilities-bukkit/src/main/java/net/omniscimus/unknownutilities/utilities/wither/WitherListener.java
+++ UnknownUtilities-bukkit/src/main/java/net/omniscimus/unknownutilities/utilities/wither/WitherListener.java
@@ -1,11 +1,15 @@
 package net.omniscimus.unknownutilities.utilities.wither;
 
-import org.bukkit.Location;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
+import java.util.UUID;
+import org.bukkit.SkullType;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Skull;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 /**
  * Listener for events related to the Wither arena.
@@ -13,6 +17,8 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 public class WitherListener implements Listener {
 
     private final WitherUtility witherUtility;
+
+    private UUID lastWitherSkullPlacer;
 
     /**
      * Creates the object.
@@ -23,17 +29,49 @@ public class WitherListener implements Listener {
 	this.witherUtility = witherUtility;
     }
 
+    /**
+     * Called by Bukkit whenever a creature spawns in the game. Used to prevent
+     * wither spawns outside of the arena.
+     *
+     * @param event the event that occurred
+     */
     @EventHandler
     public void onCreatureSpawn(CreatureSpawnEvent event) {
-	Entity entity = event.getEntity();
-	Location location = event.getLocation();
-
-	if (entity.getType() == EntityType.WITHER) {
+	if (event.getSpawnReason() == SpawnReason.BUILD_WITHER) {
 	    /* Prevent wither spawning outside of the arena */
 	    boolean inWitherArena = witherUtility.getArenaManager()
-		    .isInWitherArena(location);
-	    event.setCancelled(!inWitherArena);
+		    .isInWitherArena(event.getLocation());
+	    if (!inWitherArena) {
+		event.setCancelled(true);
+	    }
 	}
+    }
+
+    /**
+     * Called by Bukkit whenever a block is placed. Used to track the player who
+     * spawned a wither (who placed the third skull).
+     *
+     * @param event the event that occurred
+     */
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+	BlockState state = event.getBlock().getState();
+	if (state instanceof Skull) {
+	    if (((Skull) state).getSkullType() == SkullType.WITHER) {
+		lastWitherSkullPlacer = event.getPlayer().getUniqueId();
+	    }
+	}
+    }
+
+    /**
+     * Called by Bukkit whenever a player dies. Used to track when a player dies
+     * while fighting the wither.
+     *
+     * @param event the event that occurred
+     */
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+	witherUtility.removeFightingPlayer(event.getEntity());
     }
 
     /**
--- UnknownUtilities-bukkit/src/main/java/net/omniscimus/unknownutilities/utilities/wither/WitherUtility.java
+++ UnknownUtilities-bukkit/src/main/java/net/omniscimus/unknownutilities/utilities/wither/WitherUtility.java
@@ -1,21 +1,26 @@
 package net.omniscimus.unknownutilities.utilities.wither;
 
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import java.util.ArrayList;
+import java.util.UUID;
 import net.omniscimus.unknownutilities.ModuleException;
 import net.omniscimus.unknownutilities.UnknownUtilities;
 import net.omniscimus.unknownutilities.UnknownUtility;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 /**
  * This module allows players to fight the wither in an arena.
  */
 public class WitherUtility extends UnknownUtility {
-    
+
     private final UnknownUtilities plugin;
-    
+
     private ArenaManager arenaManager;
     private WitherListener listener;
-    
+
+    private ArrayList<UUID> fightingPlayers;
+
     /**
      * Constructs the object.
      *
@@ -24,10 +29,30 @@ public class WitherUtility extends UnknownUtility {
     public WitherUtility(UnknownUtilities plugin) {
 	this.plugin = plugin;
     }
+
+    /**
+     * Adds a player to the list of players who are currently fighting the
+     * Wither in the arena.
+     *
+     * @param player the player who started fighting the wither
+     */
+    public void addFightingPlayer(Player player) {
+	fightingPlayers.add(player.getUniqueId());
+    }
     
     /**
+     * Removes a player from the list of players who are currently fighting the
+     * Wither in the arena.
+     *
+     * @param player the player who stopped fighting the wither
+     */
+    public void removeFightingPlayer(Player player) {
+	fightingPlayers.remove(player.getUniqueId());
+    }
+
+    /**
      * Gets the object that handles the wither arena's location.
-     * 
+     *
      * @return ArenaManager instance
      */
     public ArenaManager getArenaManager() {
@@ -36,14 +61,16 @@ public class WitherUtility extends UnknownUtility {
 
     @Override
     protected void enable() throws ModuleException {
-	
+
+	fightingPlayers = new ArrayList<>();
+
 	/* Find WorldGuard for determining the arena region */
 	Plugin worldGuard = plugin.getServer().getPluginManager().getPlugin("WorldGuard");
 	if (!(worldGuard instanceof WorldGuardPlugin)) {
 	    throw new ModuleException("Couldn't find a compatible version of WorldGuard. WitherUtility could not be enabled.");
 	}
 	arenaManager = new ArenaManager((WorldGuardPlugin) worldGuard);
-	
+
 	listener = new WitherListener(this);
 	plugin.getServer().getPluginManager().registerEvents(listener, plugin);
     }
